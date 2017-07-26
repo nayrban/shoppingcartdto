ed1112ed2435c3b16640c7f770450d656143c000
Bryan <bacuna@gpshiring.com>
2017-07-26 10:08:35 -0600

    some changes

--- src/main/java/com/shoppingcart/mock/Customer.java
+++ src/main/java/com/shoppingcart/mock/Customer.java
@@ -2,48 +2,58 @@ package com.shoppingcart.mock;
 
 import java.io.Serializable;
 
-public class Customer implements Serializable{
-	private static final long serialVersionUID = 1L;
-	private Cart cart;
-	private String coupon;
-	private boolean isNew;
+@org.kie.api.definition.type.Description("Customer class for Shopping Cart")
+@org.kie.api.definition.type.Label("Customer")
+public class Customer implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Cart cart;
+    private String coupon;
+    private String customerType;
+    private boolean isNew;
 
-	public Customer(){
-		super();
-	}
+    public Customer() {
+        super();
+    }
 
-	public boolean getIsNew() {
-		return isNew;
-	}
+    public boolean getIsNew() {
+        return isNew;
+    }
 
-	public void addItem(Product product, int qty) {
-		cart.addItem(product, qty);
-	}
+    public void addItem(Product product, int qty) {
+        cart.addItem(product, qty);
+    }
 
-	public void setDiscount(double discount){
-		cart.setDiscount(discount);
-	}
+    public void setDiscount(double discount) {
+        cart.setDiscount(discount);
+    }
 
-	public String getCoupon() {
-		return coupon;
-	}
+    public String getCoupon() {
+        return coupon;
+    }
 
-	public void setCoupon(String coupon) {
-		this.coupon = coupon;
-	}
+    public void setCoupon(String coupon) {
+        this.coupon = coupon;
+    }
 
-	public Cart getCart() {
-		return cart;
-	}
-	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("Customer new? ")
-		   .append(isNew)
-		   .append("\nCoupon: ")
-		   .append(coupon)
-		   .append("\n")
-		   .append(cart);
-		return sb.toString();
-	}
+    public Cart getCart() {
+        return cart;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "cart=" + cart +
+                ", coupon='" + coupon + '\'' +
+                ", customerType='" + customerType + '\'' +
+                ", isNew=" + isNew +
+                '}';
+    }
 }
